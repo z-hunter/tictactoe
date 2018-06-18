6a92e71b7cfab7fe5cfee0243ecac0b4190724bd
Michael Voitovich <z-hunter@tut.by>
2018-06-18 05:33:12 +0300

    Add files via upload

---
+++ ttt.lua
@@ -0,0 +1,24 @@
+-- tictactoe game (c) Michael.Voitovich@gmail.com, 2018
+require "tools"
+
+t_Player = {
+   name="",
+   score=0,
+   check="",
+   controller="user"  -- user,AI1,AI2
+}
+
+Player1=createNew(t_Player)
+Player2=createNew(t_Player)
+
+
+
+
+
+-- Init
+Player1.name="Player #1"
+Player1.name="Player #2"
+
+-- MainGame
+
+-- End
