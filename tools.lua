0e8f4a04463700180defb430872ea21f4488be78
Michael Voitovich <z-hunter@tut.by>
2018-06-19 16:51:02 +0300

    Add files via upload

--- tools.lua
+++ tools.lua
@@ -3,7 +3,7 @@ function IsTable(s)
 end;   
 function nop() end;						-- No Operation
 
-function new(o, seen)  --> recursively copies a table's contents, ensures that metatables are preserved
+function table.deepcopy(o, seen)  --> recursively copies a table's contents, ensures that metatables are preserved
   -- Handle non-tables and previously-seen tables. |SRC: https://gist.github.com/tylerneylon/81333721109155b2d244#file-copy-lua-L84
   if not IsTable(o) then return o
   elseif seen and seen[o] then return seen[o]
@@ -16,3 +16,16 @@ function new(o, seen)  --> recursively copies a table's contents, ensures that m
   for k, v in pairs(o) do res[table.deepcopy(k, s)] = table.deepcopy(v, s) end;
   return res
 end;
+
+function createNew(what, q, init)
+	if what=="array" then
+		local ret={}
+		for i=1, q do
+			table.insert(ret,init)
+		end
+		return ret
+	else
+		return table.deepcopy(what)
+	end
+
+end
--- ttt.lua
+++ ttt.lua
@@ -1,10 +1,10 @@
 -- tictactoe game (c) Michael.Voitovich@gmail.com, 2018
 require "tools"
+os.execute("chcp 65001 >nul")
 
 t_Player = {
    name="",
    score=0,
-   check="",
    controller="user"  -- user,AI1,AI2
 }
 
@@ -12,13 +12,43 @@ Player1=createNew(t_Player)
 Player2=createNew(t_Player)
 
 
+Map={
+   init=function(S,size)
+      for y=1, size do
+	 S[y]=createNew("array",size,0)
+      end
+   end,
+   draw=function(S)
+      for y=#S,1,-1 do
+	 io.write(string.format("%2u· ",y))		 -- map Y-labels
+	 for x=1,#S do
+	    io.write(S[x][y].." ")
+	 end
+	 print()
+      end
+      io.write("   ")
+      for i=1,#S do
+	 io.write(" ·")		       
+      end
+      io.write("\n   ")      
+      for i=1,#S do
+	 io.write(string.format("%2u",i))		 -- map X-labels      
+      end
+   end
+}
+
 
 
 
 -- Init
 Player1.name="Player #1"
 Player1.name="Player #2"
+Map:init(10)
+Map[5][2]=1
+
+
 
 -- MainGame
+Map:draw()
 
 -- End
