44bcefa55cfd812aedf772e2f416bf701cf079ce
Michael Voitovich <z-hunter@tut.by>
2018-06-20 05:07:26 +0300

    + Map.NewLines mechanics

--- tools.lua
+++ tools.lua
@@ -1,11 +1,7 @@
-function IsTable(s)   
-  return type(s) == "table" ;
-end;   
-function nop() end;						-- No Operation
 
 function table.deepcopy(o, seen)  --> recursively copies a table's contents, ensures that metatables are preserved
   -- Handle non-tables and previously-seen tables. |SRC: https://gist.github.com/tylerneylon/81333721109155b2d244#file-copy-lua-L84
-  if not IsTable(o) then return o
+  if type(o) ~= "table" then return o
   elseif seen and seen[o] then return seen[o]
   end;
   -- New table; mark it as seen an copy recursively.
@@ -16,6 +12,13 @@ function table.deepcopy(o, seen)  --> recursively copies a table's contents, ens
   for k, v in pairs(o) do res[table.deepcopy(k, s)] = table.deepcopy(v, s) end;
   return res
 end;
+
+function table.clearIndexed (T)
+		for k,_ in ipairs (T) do
+			T[k]=nil
+		end
+end
+
 
 function createNew(what, q, init)
 	if what=="array" then
--- ttt.lua
+++ ttt.lua
@@ -13,16 +13,41 @@ Player2=createNew(t_Player)
 
 
 Map={
+   NewLines={		 -- list of all cells, turned into lines on previous move. Used for highlihting in .draw()
+      key = function(x,y)
+	 return tostring(x)..tostring(y)
+      end,
+      add = function(S,x,y)
+	 S.List[S.key(x,y)]={x,y}
+      end,
+      find = function(S,x,y)
+	 return S.List[S.key(x,y)]
+      end,
+      clear=function(S)	 S.List={}  end
+   },
+   
+   --
+   clear=function(S) table.clearIndexed(S) end,
+   --
    init=function(S,size)	 --> void
+      S.NewLines:clear()
+      S.LastMove={}
+      S:clear()
       for y=1, size do
 	 S[y]=createNew("array",size,0)
       end
    end,
+   
+   --
    draw=function(S)		 --> void
       for y=#S,1,-1 do
 	 io.write(string.format("%2u· ",y))		 -- map Y-labels
 	 for x=1,#S do
-	    io.write(S[x][y].." ")
+	    if S.NewLines:find(x,y) then char="x"	 -- new lines, created on last move, highlighted as scpecial characters
+	    else char=S[x][y]				 
+	    end
+	    if S.LastMove[1]== x and S.LastMove[2]== y then char = "X" end     
+	    io.write(char.." ")
 	 end
 	 print()
       end
@@ -34,39 +59,64 @@ Map={
       for i=1,#S do
 	 io.write(string.format("%2u",i))		 -- map X-labels      
       end
+      print()
    end,
+   
+   --
    makeMove=function(S,p,x,y)	 -- p:player's token; x,y:coordinates --> hits or nil 
-
-      local function calcSame(...)		--> 1: if all args are the same (equal) / 0: if not
-	 first=arg[1]
-	 for _, curr in ipairs(arg) do
-	    if first ~= curr then return 0 end
-	 end
-	 return 1
+     
+      local function calcSame(A)		--> 1: if all args are the same (equal) / 0: if not
+	    first=A[1]
+	    for _, curr in ipairs(A) do
+	       if first ~= curr then return 0 end
+	    end
+	    return 1
       end
 
-      local function calcHit(...)  -- arg: {x1,y1},{x2,y2}..{xn,yn} coordinates of cells -> 1/0
-	 Cells={}
+      local function makeNewLine(C)
+	 for _, v in ipairs(C) do
+	    S.NewLines:add (v[1],v[2])
+	 end
+      end
+      
+      local function calcHit(...)  -- arg: {x1,y1},{x2,y2}..{xn,yn} coordinates of cells --> hits (1/0), Coords: table of {x,y} for every turned into line cell
+	 Cells, Coords = {},{}
 	 for _, A in ipairs(arg) do					   -- collect all cells content and eliminate incorrect 
 	    if A[1]<1 or A[1]>#S or A[2]<1 or A[2]>#S  then
-	       return 0						   -- cell is out of range
+	       return 0							   -- cell is out of range
 	    end	  
 	    c = S[A[1]][A[2]]						   -- с = content of the next cell from arg list
 	    if c>2  then return 0 end					   -- cell is already part of some line	    
 	    table.insert(Cells,c)
+	    table.insert(Coords,{A[1],A[2]})
 	 end	 
-	 return calcSame(unpack(Cells))
+	 hit=calcSame(Cells)
+	 if hit>0 then makeNewLine(Coords) end
+	 return hit
       end
+      
 
       if S[x][y] ~= 0 then return nil					   -- it's accepted to only put token on empty field
       elseif x<1 or x>#S or y<1 or y>#S then return nil			   -- cell is out of Map range
       end
+      
+      S.LastMove={x,y}
       S[x][y]=p								   -- place token on Map 
       hits = calcHit({x-1,y},{x,y},{x+1,y})				   -- calculate hits for horizontal line
       hits = hits + calcHit({x-1,y+1},{x,y},{x+1,y-1})			    -- left-rightdown diagonal
       hits = hits + calcHit({x,y+1},{x,y},{x,y-1})			   -- vertical
       hits = hits + calcHit({x-1,y-1},{x,y},{x+1,y+1})			   -- left-rightup diagonal
+
       return hits
+
+   end,
+
+   endMove=function(S)
+      for _,V in pairs(S.NewLines.List) do
+	 S[V[1]][V[2]]=S[V[1]][V[2]]+2
+      end
+      S.NewLines:clear()
+      S.LastMove={}
    end,
 }
 
@@ -86,6 +136,8 @@ print(Map:makeMove(1,6,2))
 
 -- MainGame
 Map:draw()
-
+Map:endMove()
+Map:draw()
 -- End
+
 
